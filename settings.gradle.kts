6b3465d5318bba8e81f4c5b9a19b88ad38506a8e
Michael <LeyuanMa5@gmail.com>
2024-12-16 10:11:48 -0600

    fixed the  MPAndroidChart outdated dependency

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,11 @@ dependencyResolutionManagement {
 
 rootProject.name = "My Application"
 include(":app")
- 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
